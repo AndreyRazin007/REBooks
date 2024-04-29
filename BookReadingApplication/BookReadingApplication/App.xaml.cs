15eb37f7d0c28a02042b2abfbc50cf20f7025f5b
SimpleFreak <ISFreak@yandex.ru>
2024-04-29 13:09:48 +0500

    Added release version.

--- BookReadingApplication/BookReadingApplication/App.xaml.cs
+++ BookReadingApplication/BookReadingApplication/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using System.Configuration;
+using System.Data;
 using System.Windows;
-using System.Windows.Media.Imaging;
 
 namespace BookReadingApplication
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
     }
+
 }
---
+++ BookReadingApplication/BookReadingApplication/SelectTheme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace BookReadingApplication;
+
+public class SelectTheme
+{
+    public static void ChangeTheme(Uri themeUri)
+    {
+        ResourceDictionary theme = new()
+        {
+            Source = themeUri
+        };
+
+        Application.Current.Resources.Clear();
+        Application.Current.Resources.MergedDictionaries.Add(theme);
+    }
+}
